2f0b64c229c802690158c2b8d988ac33609e242e
Josiah <josiah.pilon@gmail.com>
2025-07-02 21:05:52 -0700

    db: adds migration to create Tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_29_120000) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
     t.boolean "completed", default: false
+    t.integer "action_category", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
