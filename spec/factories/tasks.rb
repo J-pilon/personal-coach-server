ed0f662d7fd476020e29e7a4e58a14f7bab68457
Josiah <josiah.pilon@gmail.com>
2025-07-02 21:06:35 -0700

    test: tasks crud operations

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+  factory :task do
+    title { Faker::Lorem.sentence(word_count: 3) }
+    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    completed { false }
+    action_category { Task.action_categories.keys.sample }
+
+    trait :completed do
+      completed { true }
+    end
+
+    trait :with_long_title do
+      title { Faker::Lorem.sentence(word_count: 10) }
+    end
+
+    trait :with_long_description do
+      description { Faker::Lorem.paragraph(sentence_count: 5) }
+    end
+  end
+end
