8b1d9e3ba05c50ae2d413c1bc383022018ce50bc
Josiah <josiah.pilon@gmail.com>
2025-07-21 21:11:30 -0700

    feat: adds basic auth to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,23 @@
 
 class ApplicationController < ActionController::API
 
+  private
+
   def current_user
-    User.first
+    @current_user ||= User.first
+  end
+
+  def authenticate_user!
+    # TODO
+    # This would typically use JWT or session-based authentication
+    # For now, we'll assume the user is authenticated via a header
+    user_id = request.headers['X-User-ID']
+
+    unless user_id && User.exists?(id: user_id)
+      render json: { error: 'Authentication required' }, status: :unauthorized
+      return
+    end
+
+    @current_user = User.find(user_id)
   end
 end
