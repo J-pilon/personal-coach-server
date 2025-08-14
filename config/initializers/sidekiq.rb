f12829b23b1ff3497c1610f5d914903ec5a842d3
Josiah <josiah.pilon@gmail.com>
2025-08-14 09:29:15 -0700

    fix: disables SSL for heroku redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,22 @@
 
 require 'sidekiq'
 require 'sidekiq-status'
+require 'openssl'
 
-# Configure Sidekiq server middleware
 Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+
   # Configure Sidekiq::Status server middleware
   # This enables job status tracking on the server side
   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes.to_i
+end
 
-  # Add any other server middleware here if needed
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
