5f5674f3f4f72995c988b2643fe640f2040792e4
Josiah <josiah.pilon@gmail.com>
2025-12-02 11:33:00 -0800

    adds notification services for daily reminder and engagement reminder

---
+++ lib/notifications/daily_reminder_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Notifications
+  class DailyReminderService
+    def notification_type
+      'daily_reminder'
+    end
+
+    def notification_title
+      'Stay on Track! 💪'
+    end
+
+    def notification_body
+      "Can't accomplish your goal by not chipping away at it day by day"
+    end
+
+    def notification_data
+      {
+        type: 'daily_reminder',
+        screen: 'tasks'
+      }
+    end
+  end
+end
