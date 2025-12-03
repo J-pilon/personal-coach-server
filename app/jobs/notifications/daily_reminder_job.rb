338587d078ed009d204b6ba89b7723b09493f376
Josiah <josiah.pilon@gmail.com>
2025-12-02 20:00:39 -0800

    adds Notifications::DailyReminderJob

---
+++ app/jobs/notifications/daily_reminder_job.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Notifications::DailyReminderJob < ApplicationJob
+  queue_as :notifications
+
+  retry_on StandardError, wait: :polynomially_longer, attempts: 2
+
+  def perform(profile_id)
+    profile = Profile.find(profile_id)
+    Notifications::DailyReminderService.new(profile).call
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.warn("DailyReminderJob: Profile not found: #{profile_id}")
+  end
+end
