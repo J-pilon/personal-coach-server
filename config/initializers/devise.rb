8b35b82f25178d2aa40d2aeae84a36427781526f
Josiah <josiah.pilon@gmail.com>
2026-05-07 12:26:27 -0700

    fix: rename FROM_MAIL_ADDRESS env var to MAIL_FROM_ADDRESS

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV.fetch('FROM_MAIL_ADDRESS', 'no-reply@personal-coach.app')
+  config.mailer_sender = ENV.fetch('MAIL_FROM_ADDRESS', 'no-reply@personal-coach.app')
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
